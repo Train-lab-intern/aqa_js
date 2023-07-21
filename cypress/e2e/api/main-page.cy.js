e52475a4df03ac8fa23e6b63cacf56783dee6e0b
nataliasiutsova <tashaklimenkova@gmail.com>
2023-07-21 15:06:37 +0300

    DB config

--- cypress/e2e/api/main-page.cy.js
+++ cypress/e2e/api/main-page.cy.js
@@ -1,10 +1,13 @@
-describe('Backend main page', () => {
-  it('Check main page data', () => {
-    cy.fixture('main-page-data.json').then((data) => {
-      cy.request('GET', '/1').then((res) => {
-        expect(res.status).to.eq(200);
-        expect(res.body).to.deep.equal(data);
-      });
+describe("Backend main page", () => {
+  it("Check ST-1.1 text", () => {
+    cy.request(
+      "GET",
+      "https://back-test-4zwpv.ondigitalocean.app/front/pages/1"
+    ).then((res) => {
+      cy.task(
+        "connectDB",
+        `SELECT text FROM frontend_data WHERE frontend_id='1.1'`
+      ).then((result) => expect(res.body[1.1]).to.equal(result[0].text));
     });
   });
 });
