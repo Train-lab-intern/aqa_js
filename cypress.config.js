069ab38d949ebceb50381caea79d81ee48c98212
Anna <anna_vyalova@yahoo.com>
2023-07-24 22:50:18 +0200

    Update API Test

--- cypress.config.js
+++ cypress.config.js
@@ -1,35 +1,26 @@
-const { defineConfig } = require("cypress");
-const { Client } = require("pg");
-
-require("dotenv").config();
+const { defineConfig } = require("cypress")
+const { Pool } = require("pg")
+const dotenv = require("dotenv")
+dotenv.config()
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://front-test-a2ykv.ondigitalocean.app/",
     setupNodeEvents(on, config) {
       on("task", {
-        async connectDB(query) {
-          const client = new Client({
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            port: process.env.DB_PORT,
-          });
-          await client.connect();
-          await client.connect();
-          const res = await client.query(query);
-          await client.end();
-          return res.rows;
+        async connectDB(sql) {
+          const pool = new Pool({
+            user: process.env.DBUSER,
+            password: process.env.DBPASSWORD,
+            host: process.env.DBHOST,
+            database: process.env.DBNAME,
+            ssl: { require: true },
+            port: 5432,
+          })
+          const result = await pool.query(sql)
+          await pool.end()
+          return result
         },
-      });
-    },
-    DB: {
-      user: process.env.USER_DB,
-      password: process.env.PASSWORD_DB,
-      host: "localhost",
-      database: process.env.NAME_DB,
-      port: "5432",
+      })
     },
   },
 })
